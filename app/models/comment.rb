b0da1b8cd05ed589be7e8b46742079cb58b58548
Jason Vang <jvang@Jasons-MacBook-Air.local>
2023-12-18 23:29:10 -0600

    added test to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,14 +4,5 @@ class Comment < ApplicationRecord
   include Visible
 
   belongs_to :article
-
-  class CommentTest < ActiveSupport::TestCase
-    test "should belong to an article" do
-      article = Article.create(title: "Valid Title", body: "This body is going to have more than 10 words here.")
-      comment = Comment.new(body: "This is a comment", article: article)
   
-      assert comment.save, "Comment should be saved with an article"
-      assert_equal article, comment.article, "Comment should belong to the article"
-    end
   end
-end
