9bfdda7ec687e36e966591a23cf00e4873b8ac95
Jason Vang <jvang@Jasons-MacBook-Air.local>
2023-12-19 23:56:05 -0600

    added delete test for controllers: categories and articles

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -6,4 +6,19 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  describe 'DELETE #destroy' do
+  let(:category) { create(:category) }
+  let(:article) { create(:article, category: category) }
+
+  it 'destroys the requested article' do
+    expect {
+      delete :destroy, params: { category_id: category.id, id: article.id }
+    }.to change(Article, :count).by(-1)
+  end
+
+  it 'redirects to the root path' do
+    delete :destroy, params: { category_id: category.id, id: article.id }
+    expect(response).to redirect_to(root_path)
+  end
+end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,4 +6,18 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  describe 'DELETE #destroy' do
+    it 'destroys the requested category' do
+      category = Category.create(valid_attributes)
+      expect {
+        delete :destroy, params: { id: category.to_param }
+      }.to change(Category, :count).by(-1)
+    end
+
+    it 'redirects to the root path' do
+      category = Category.create(valid_attributes)
+      delete :destroy, params: { id: category.to_param }
+      expect(response).to redirect_to(root_path)
+    end
+  end
 end
